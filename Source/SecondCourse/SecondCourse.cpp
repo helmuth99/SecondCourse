2c539130e9e79a93d6a2be4c0f851dead505eb29
helmuth99 <73831413+helmuth99@users.noreply.github.com>
2022-03-09 00:23:24 +0100

    first upload
    first upload

---
+++ Source/SecondCourse/SecondCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SecondCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecondCourse, "SecondCourse" );
